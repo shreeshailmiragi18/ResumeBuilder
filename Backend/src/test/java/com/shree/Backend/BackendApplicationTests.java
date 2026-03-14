d03af778d2648bbcfd64e65c5cb675c31b5917de
shreeshailmiragi18 <shreeshailbm18@gmail.com>
2026-03-15 04:02:11 +0530

    backend setup

---
+++ Backend/src/test/java/com/shree/Backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shree.Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
