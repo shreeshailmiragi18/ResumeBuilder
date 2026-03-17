acebee653f37f3168c509c71d851500164c4c931
shreeshailmiragi18 <shreeshailbm18@gmail.com>
2026-03-18 02:42:39 +0530

    implemented the email verification service and sent first email

--- Backend/src/main/java/com/shree/Backend/service/AuthService.java
+++ Backend/src/main/java/com/shree/Backend/service/AuthService.java
@@ -7,6 +7,7 @@ import com.shree.Backend.exceptions.ResourceExistsException;
 import com.shree.Backend.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
@@ -18,6 +19,11 @@ import java.util.UUID;
 public class AuthService {
 
     private final UserRepository userRepository;
+    private final EmailService emailService;
+
+
+    @Value("${app.base,url:http://localhost:8080 }")
+    private String appUrl;
 
     public AuthResponse register(RegisterRequest request) {
         log.info("Inside AuthService: register() {}",request);// displays the logs which makes the debug easy
@@ -38,6 +44,7 @@ public class AuthService {
                 .build();
 
         userRepository.save(user);
+        sendVerificationEmail(user);
 
         return AuthResponse.builder()
                 .id(user.getId())
@@ -49,5 +56,26 @@ public class AuthService {
                 .createdAt(user.getCreatedAt())
                 .updatedAt(user.getUpdatedAt())
                 .build();
+    }
+
+    private void sendVerificationEmail(User user) {
+        try{
+            String link = appUrl + "/api/auth/verify-email?token="+user.getVerificationToken();
+            String html = "<div style='font-family:sans-serif'>" +
+                    "<h2>Verify your email</h2>" +
+                    "<p>Hi " + user.getName() + ", please confirm your email to activate your account.</p>" +
+                    "<p>" +
+                    "<a href='" + link + "' " +
+                    "style='display:inline-block;padding:10px 16px;background:#6366f1;color:#fff;border-radius:6px;text-decoration:none;'>"
+                    + "Verify Email</a>" +
+                    "</p>" +
+                    "<p>Or copy this link:" + link + "</p>" +
+                    "<p>This link expires in 24 hours.</p>" +
+                    "</div>";
+            emailService.sendHtmlEmail(user.getEmail(),"verify your email ", html );
+
+        }catch (Exception e){
+            throw new RuntimeException("Failed to send verification email: "+e.getMessage());
+        }
     }
 }
